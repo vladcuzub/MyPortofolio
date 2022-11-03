cbb9f7085fd6c31650b7024af08bba00d44113ce
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2022-11-03 17:30:07 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+const sections = document.querySelectorAll('.section');
+const secBtns = document.querySelectorAll('.controls');
+const secBtn = document.querySelectorAll('.control');
+const allSelections = document.querySelector('.main-content');
+
+
+function PageTransition() {
+      //Buttons click active class
+      for (let i = 0; i < secBtn.length; i++){
+            secBtn[i].addEventListener('click', function() {
+                  let currentBtn = document.querySelectorAll('.active-btn');
+                 
+                  currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+                  this.className += ' active-btn';
+            })
+      }
+      //Sections active class
+      allSelections.addEventListener('click', (e) => {
+          const id =  e.target.dataset.id;
+          if(id){
+            //remove selected from the other btns
+            secBtns.forEach((btn) => {
+                  btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+            //hide other sections 
+            sections.forEach((section)=>{
+                  section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+          }
+      })
+            
+}
+PageTransition();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
